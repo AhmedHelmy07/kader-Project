4a4fdbfe67253b051eb524f2165ade446180e1f4
Ahmed Helmy <136022635+AhmedHelmy07@users.noreply.github.com>
2025-09-30 07:21:05 +0000

    update logo path

--- components/icons/KaderLogo.tsx
+++ components/icons/KaderLogo.tsx
@@ -6,6 +6,6 @@ export const KaderLogo: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <image href="/images/logo.png" x="0" y="0" width="100" height="100" />
+    <image href={require('../../images/logo.png')} x="0" y="0" width="100" height="100" />
   </svg>
 );
