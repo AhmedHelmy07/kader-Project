daf303ca03eecd23639d81e5ad406f51a1bdb2b1
Ahmed Helmy <136022635+AhmedHelmy07@users.noreply.github.com>
2025-09-30 07:26:52 +0000

    fix logo

--- components/icons/KaderLogo.tsx
+++ components/icons/KaderLogo.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-export const KaderLogo: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg
-    viewBox="0 0 100 100"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <image href={require('../../images/logo.png')} x="0" y="0" width="100" height="100" />
-  </svg>
-);
+export const KaderLogo: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = (props) => {
+  return <img src={require('../../images/logo.png')} alt="Kader Logo" {...props} />;
+};
