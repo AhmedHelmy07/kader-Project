8dc6db89e0956dd28c20c731811c9e30263b7b98
goobeereee-lab <goobeereee@gmail.com>
2025-11-27 22:01:24 +0000

    modified:   components/icons/KaderLogo.tsx

--- components/icons/KaderLogo.tsx
+++ components/icons/KaderLogo.tsx
@@ -2,5 +2,12 @@ import React from 'react';
 import logo from '../../images/logo.png';
 
 export const KaderLogo: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = (props) => {
-  return <img src={logo} alt="Kader Logo" {...props} />;
+  return (
+    <img 
+      src={logo} 
+      alt="Kader Logo" 
+      className="object-contain object-center"
+      {...props} 
+    />
+  );
 };
